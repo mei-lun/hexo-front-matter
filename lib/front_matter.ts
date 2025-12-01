f99d078502a6af25a48852ef73b9f49cd5e4f613
mei-lun <54898725+mei-lun@users.noreply.github.com>
2025-12-01 23:33:41 +0800

    Update parse function parameter naming convention
    Signed-off-by: mei-lun <54898725+mei-lun@users.noreply.github.com>

--- lib/front_matter.ts
+++ lib/front_matter.ts
@@ -37,7 +37,7 @@ interface ParseOptions {
   defaultTimeZone?: string;
 }
 
-function parse(str: string, _options: ParseOptions = {}) {
+function parse(str: string, /* eslint-disable-next-line @typescript-eslint/no-unused-vars */ options: ParseOptions = {}) {
   if (typeof str !== 'string') throw new TypeError('str is required!');
 
   const splitData = split(str);
